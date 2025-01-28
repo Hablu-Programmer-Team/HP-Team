a767aaa086ccd2358333b7d8492309253f90c534
EyachirArafat <me.eyachirarafat@gmail.com>
2025-01-28 17:48:07 +0600

    first print

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div>
-      <p>Hello HP Team</p>
+    <div className='flex w-full justify-center h-screen items-center'>
+      <p className='text-4xl text-indigo-500'>Hello HP Team</p>
     </div>
   )
 }
