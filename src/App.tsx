6b89aed17b418bd2e9181757354a0b5a604ee646
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2025-01-29 13:54:37 +0600

    refactor: update ESLint configuration and improve code formatting; add new dev dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Dashboard, Login, Profile, Registration } from "@/components/pages";
 import { Route, Routes } from "react-router";
 
+// test parpas function
+export function test() {}
+
 export const App = () => {
   return (
     <Routes>
