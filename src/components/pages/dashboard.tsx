9e26573cb54d506b38e445afe1389f3bac598b70
sayed udoy <umudoy364@gmail.com>
2025-01-31 20:45:47 +0500

    done

--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -1,10 +1,1 @@
-import { Button } from "../team/Udoy/button";
-export const Dashboard = () => (
-  <div className="h-screen grid place-content-center">
-    <div className="flex gap-5">
-      <Button variant="ghost" font="b" textSize="2xl">
-        + Text
-      </Button>
-    </div>
-  </div>
-);
+export const Dashboard = () => <div>Dashboards</div>;
