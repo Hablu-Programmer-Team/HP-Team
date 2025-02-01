ace7012ec335ae15cee0ad9f218c0ac57c972b54
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2025-02-01 09:50:43 +0600

    Add class-variance-authority dependency and refactor Button component

--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Button } from "../team/Udoy/button";
 
 export const Dashboard = () => (
-  <div className="bg-gray-300 h-screen w-full flex justify-center items-center flex-col">
+  <div className="bg-gray-400 h-screen w-full flex justify-center items-center flex-col">
     <Button>Primary</Button>
   </div>
 );
--- src/components/team/Udoy/button.tsx
+++ src/components/team/Udoy/button.tsx
@@ -1,18 +1,10 @@
-import type { ButtonHTMLAttributes, FC } from "react";
+import { cn } from "@/lib/utils/cn";
+import { forwardRef, type ButtonHTMLAttributes } from "react";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
 
-interface IProps extends ButtonProps {
-  variant?: string;
-  size?: string;
-  flex?: boolean;
-  disabledClass?: string;
-  hover?: string;
-  round?: string;
-  font?: string;
-  colors?: string;
-  textSize?: string;
-}
 // size styles: by default button size will be response style
 const sizeStyles: Record<string, string> = {
   sm: "px-[1rem] py-[0.375rem]",
@@ -28,50 +20,87 @@ const variantStyles: Record<string, string> = {
   ghost: "border border-gray-300",
 };
 
-export const Button: FC<IProps> = (props) => {
-  const {
-    children,
-    variant = "primary",
-    size = "responsive",
-    disabled,
-    hover = "hover:opacity-80",
-    flex,
-    round,
-    colors,
-    font,
-    textSize,
-    className = "",
-    disabledClass = "opacity-50 border border-gray-400 bg-gray-400",
-    ...others
-  } = props;
+// export const Button2: FC<IProps> = (props) => {
+//   const {
+//     children,
+//     variant = "primary",
+//     size = "responsive",
+//     disabled,
+//     // hover = "hover:opacity-80",
+//     flex,
+//     round,
+//     colors,
+//     font,
+//     textSize,
+//     className = "",
+//     disabledClass = "opacity-50 border border-gray-400 bg-gray-400",
+//     ...others
+//   } = props;
 
-  const hoverStyle = hover === "auto" ? "hover:scale-105" : hover;
-  const roundedStyle =
-    round === "sm" ? "rounded-sm" : round === "md" ? "rounded-md" : round;
-  const flexStyle = "flex items-center justify-center text-nowrap gap-1";
-  const textColor = colors ? colors : "text-black";
-  const fontBoldStyle =
-    font === "b" ? "font-bold" : font === "ex" ? "font-extrabold" : font;
-  const generateClassNames = (...classes: unknown[]): string =>
-    classes.filter(Boolean).join(" ");
+//   // const hoverStyle = hover === "auto" ? "hover:scale-105" : hover;
+//   const roundedStyle =
+//     round === "sm" ? "rounded-sm" : round === "md" ? "rounded-md" : round;
+//   const flexStyle = "flex items-center justify-center text-nowrap gap-1";
+//   const textColor = colors ? colors : "text-black";
+//   const fontBoldStyle =
+//     font === "b" ? "font-bold" : font === "ex" ? "font-extrabold" : font;
+//   const generateClassNames = (...classes: unknown[]): string =>
+//     classes.filter(Boolean).join(" ");
 
-  const btnClasses =
-    className ||
-    generateClassNames(
-      fontBoldStyle,
-      textColor,
-      textSize || "text-[0.8125rem]",
-      flex && flexStyle,
-      roundedStyle || "rounded-full",
-      hoverStyle,
-      sizeStyles[size] || size,
-      disabled ? disabledClass : variantStyles[variant] ?? variant,
-      disabled ? "cursor-not-allowed" : "cursor-pointer",
-      "transition duration-200"
-    );
-  return (
-    <button className={btnClasses} {...others}>
-      {children}
-    </button>
-  );
-};
+//   const btnClasses =
+//     className ||
+//     generateClassNames(
+//       fontBoldStyle,
+//       textColor,
+//       textSize || "text-[0.8125rem]",
+//       flex && flexStyle,
+//       roundedStyle || "rounded-full",
+//       // hoverStyle,
+//       sizeStyles[size] || size,
+//       disabled ? disabledClass : variantStyles[variant] ?? variant,
+//       disabled ? "cursor-not-allowed" : "cursor-pointer",
+//       "transition duration-200"
+//     );
+//   return (
+//     <button className={cn(btnClasses)} {...others}>
+//       {children}
+//     </button>
+//   );
+// };
+
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, ...props }, ref) => (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props}
+    />
+  )
+);
+Button.displayName = "Button";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-link-700 text-black shadow hover:bg-link-700/90",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        accent: "hover:bg-primary-500 ",
+        outline:
+          "border bg-background shadow-sm border border-link-700 text-link-700 hover:bg-link-700 ",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "px-4 py-2",
+        sm: " rounded-md px-3 text-xs",
+        lg: " rounded-md px-8",
+        icon: "size-9",
+      },
+    },
+    defaultVariants: { variant: "default", size: "default" },
+  }
+);
