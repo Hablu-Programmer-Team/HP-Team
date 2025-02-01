e421dd66c492c48bb2a11da6faabcd5590ca6231
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2025-02-01 08:29:23 +0600

    Add Button component and update Dashboard layout

--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -1,1 +1,7 @@
-export const Dashboard = () => <div>Dashboards</div>;
+import { Button } from "../team/Udoy/button";
+
+export const Dashboard = () => (
+  <div className="bg-gray-300 h-screen w-full flex justify-center items-center flex-col">
+    <Button>Primary</Button>
+  </div>
+);
