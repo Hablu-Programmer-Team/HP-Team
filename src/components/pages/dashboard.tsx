ddf6248550ec024d2a1c877ddc772ac9848640ce
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2025-02-01 19:50:37 +0600

    Remove Button component and simplify Dashboard to a basic structure

--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -1,11 +1,1 @@
-import { Button } from "../team/Udoy/button";
-
-export const Dashboard = () => (
-
-<div className="flex justify-center items-center bg-slate-900 min-h-52">
-<Button variant={"secondary"} size="sm" radius="full" >
-      Primary
- </Button>
-</div>
-
-);
+export const Dashboard = () => <div>dashboard</div>;
