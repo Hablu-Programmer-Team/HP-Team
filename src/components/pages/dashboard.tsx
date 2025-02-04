fc0651a46343e53505d9e1bfaee01cb19a797613
sayed udoy <umudoy364@gmail.com>
2025-02-04 15:51:08 +0500

    done

--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -1,9 +1,1 @@
-import { WatchOutput } from "../team/Eyachir/watch-output";
-import { AddTasks } from "../team/Mamun/startTask";
-
-export const Dashboard = () => (
-  <div>
-    <AddTasks btnName="me" cardName="Team Task" />
-    <WatchOutput />
-  </div>
-);
+export const Dashboard = () => <div>Dashboard</div>;
