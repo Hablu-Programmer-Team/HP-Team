420b6cb4088dc44601629e7dcb1536e17e1ce43a
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2025-02-06 20:27:18 +0600

    btn varient

--- src/components/team/Alamin/profile-page/profile.tsx
+++ src/components/team/Alamin/profile-page/profile.tsx
@@ -1,11 +1,9 @@
 import { Sidebar } from "./profile-sidebar/sidebar";
 
-export const Profile = () => {
-  return (
-    <section className="bg-[#030712]">
-      <div className="container mx-auto h-screen pt-10">
-        <Sidebar />
-      </div>
-    </section>
-  );
-};
+export const Profile = () => (
+  <section className="bg-[#030712] w-full">
+    <div className="container mx-auto h-screen pt-10">
+      <Sidebar />
+    </div>
+  </section>
+);
